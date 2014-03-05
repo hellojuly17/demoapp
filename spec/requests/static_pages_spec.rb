427541bdc8f7c9ec2d0b5597399c3681d33dbefe
Carmela <hellojuly17@gmail.com>
2014-03-05 03:19:42 +0000

    Changed home page path

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,29 +1,17 @@
 require 'spec_helper'
  
 describe "Static pages" do
- 
-  describe "Home page" do
- 
-    it "should have the content 'Demo App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Demo App')
-    end
-    require 'spec_helper'
- 
-describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Demo App'" do
-      visit root path
+      visit root_path
       expect(page).to have_content('Demo App')
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit root path
+      visit help_path
       expect(page).to have_content('Help')
     end
   end   
-end
-  end
 end
